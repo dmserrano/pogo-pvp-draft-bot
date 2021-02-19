069b0dd8c56bce189770f73d57f2ad29852fdbe4
Dominic Serrano <dserrano@atiba.com>
2021-02-19 12:53:36 -0600

    Added latency timestamps to the ping command

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,12 @@ module.exports = {
     name: "ping",
     description: "Ping!",
     execute(message) {
-        message.channel.send("Pong.");
+        const latency = Date.now() - message.createdTimestamp;
+        const apiLatency = Math.round(message.client.ws.ping);
+
+        message.channel.send(
+            `🏓 Latency is ${latency}ms. API Latency is ${apiLatency}ms`,
+        );
     },
     cooldown: 5,
 };
